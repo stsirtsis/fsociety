e1d049884dc0fbba0c5370e0e590553e111f5e22
aggelosdani <aggelosdani@gmail.com>
2017-12-06 20:24:02 +0200

    Parent Repository

---
+++ backend/src/main/java/gr/ntua/ece/softeng/ParentRepository.java
@@ -0,0 +1,9 @@
+package gr.ntua.ece.softeng;
+import org.springframework.data.repository.CrudRepository;
+
+import gr.ntua.ece.softeng.Parent;
+
+
+public interface ParentRepository extends CrudRepository<Parent, Long> {
+
+}

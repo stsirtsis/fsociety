c33257cd7312bebd131e779ae60471bfcaa16bae
stsirtsis <stsirtsis@gmail.com>
2017-12-17 23:01:19 +0200

    Created front page and toolbar.

--- backend/frontend/src/app/app-routing.module.ts
+++ backend/frontend/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ParentRegisterComponent} from './parent-register-component/parent-register-component.component';
 import { ProviderRegisterComponent } from './provider-register/provider-register.component';
-    
+import { FrontPageComponent } from './front-page/front-page.component';
+ 
 const appRoutes: Routes = [
+  { path: '', component: FrontPageComponent },
   { path: 'parent_register', component: ParentRegisterComponent },
   { path: 'provider_register', component: ProviderRegisterComponent }    
 ];
--- backend/frontend/src/app/app.module.ts
+++ backend/frontend/src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { routing } from './app-routing.module';
+import { FrontPageComponent } from './front-page/front-page.component';
 import { ParentRegisterComponent } from './parent-register-component/parent-register-component.component';
 import { ProviderRegisterComponent } from './provider-register/provider-register.component';
-
+import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
+import { MatToolbarModule, MatButtonModule, MatCheckboxModule, MatGridListModule, MatInputModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FrontPageComponent,
+    MainToolbarComponent,
     ParentRegisterComponent,
     ProviderRegisterComponent
   ],
   imports: [
     BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatInputModule,
+    MatIconModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
     routing
   ],
   providers: [],

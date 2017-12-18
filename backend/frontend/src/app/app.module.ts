f8de6566bade886bde9464998eec003957499bc6
stsirtsis <stsirtsis@gmail.com>
2017-12-18 10:29:21 +0200

    Created Material module and footer.

--- backend/frontend/src/app/app.module.ts
+++ backend/frontend/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module'
 
 import { AppComponent } from './app.component';
 import { routing } from './app-routing.module';
+import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { ParentRegisterComponent } from './parent-register-component/parent-register-component.component';
 import { ProviderRegisterComponent } from './provider-register/provider-register.component';
-import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
-import { MatToolbarModule, MatButtonModule, MatCheckboxModule, MatGridListModule, MatInputModule, MatIconModule } from '@angular/material';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,16 +17,12 @@ import { MatToolbarModule, MatButtonModule, MatCheckboxModule, MatGridListModule
     FrontPageComponent,
     MainToolbarComponent,
     ParentRegisterComponent,
-    ProviderRegisterComponent
+    ProviderRegisterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatGridListModule,
-    MatInputModule,
-    MatIconModule,
-    MatToolbarModule,
+    MaterialModule,
     BrowserAnimationsModule,
     routing
   ],

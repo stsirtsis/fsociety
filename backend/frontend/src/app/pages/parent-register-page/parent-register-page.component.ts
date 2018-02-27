df37d9a0dab8d771331dc83ea50b99c23a183ec7
christos <xristospier@gmail.com>
2018-02-27 20:33:02 +0200

    Register Pages update

--- backend/frontend/src/app/pages/parent-register-page/parent-register-page.component.ts
+++ backend/frontend/src/app/pages/parent-register-page/parent-register-page.component.ts
@@ -20,6 +20,16 @@ export class ParentRegisterPageComponent implements OnInit {
   customResponse: CustomResponse
   error = '';
   error_mail= '';
+  error_tel='';
+  error_iban='';
+  error_ch='';
+  error_nul='';
+  checkbox = false;
+  error_fir='';
+  error_las='';
+  error_user='';
+  error_pass='';
+  a=1;
   constructor(private parentService: ParentService,private router: Router,
     private userService: UserService, private auth: AuthenticationService,
     ) {}
@@ -45,26 +55,82 @@ export class ParentRegisterPageComponent implements OnInit {
 
         );
     }
-    
-      onSubmit() {
-        this.parentService.createParent(this.parent)
-          .subscribe(
-          res => {
-           if(this.parent.email.indexOf("@") == -1){
-                this.error_mail = 'Incorrect email';
-           }
-           else {
-                this.customResponse = res.body;
-                this.message = this.customResponse.message;
-                console.log(this.message);
-                if(this.message == 'ok with post from parent')
-                    this.login();
-           }
-          }),
-          () => {
-            console.log("in ()");
-          }
-      }
-    
-}
+    check(){
+    this.checkbox = true;
+  }
 
+  onSubmit() {
+    this.error_ch='';
+    if(this.checkbox){
+    this.error_mail='';
+    this.error_tel='' ;
+    this.error_iban='';
+    this.error_fir='';
+    this.error_las='';
+    this.error_user='';
+    this.error_pass='';
+    this.a=1;
+    if (this.parent.firstName == '')
+    {
+      this.error_fir="Field first name must be complected";
+      this.a=0;
+    }
+    if (this.parent.lastName == '')
+    {
+      this.error_las="Field last name must be complected";
+      this.a=0;
+    }
+    if (this.parent.username == '')
+    {
+      this.error_user="Field usernane must be complected";
+      this.a=0;
+    }
+    if (this.parent.password.length <8)
+    {
+      this.error_pass="Password must be at least 8 characters";
+      this.a=0;
+    }
+
+    if(this.parent.email.indexOf("@") == -1){
+      this.error_mail = 'Incorrect email';
+      this.a=0;
+    }
+    if (this.parent.phoneNumber.length != 10)
+    {
+      this.error_tel= 'Incorrect Telephone Number (10 digits needed)';
+      this.a=0;
+    }
+    if (this.parent.debitCard.length != 16)
+    {
+      this.error_iban= 'Incorrect Iban (16 digits needed)';
+      this.a=0;
+    }
+       else if (this.a==1){
+         this.parentService.createParent(this.parent)
+             .subscribe(
+             res => {
+              if(this.parent.email.indexOf("@") == -1){
+                   this.error_mail = 'Incorrect email';
+              }
+              else {
+                   this.customResponse = res.body;
+                   this.message = this.customResponse.message;
+                   console.log(this.message);
+                   if(this.message == 'ok with post from parent')
+                       this.login();
+              }
+             }),
+             () => {
+               console.log("in ()");
+             }
+        }
+
+   }
+   else{
+     this.error_ch= 'Terms of use must be accepted';
+     this.router.navigate(['parent-register']);
+     this.a=0;
+   }
+   }
+
+ }
--- backend/frontend/src/app/pages/provider-register-page/provider-register-page.component.ts
+++ backend/frontend/src/app/pages/provider-register-page/provider-register-page.component.ts
@@ -21,7 +21,18 @@ customResponse: CustomResponse;
 message: string;
 error = '';
 error_mail= '';
+error_tel='';
+error_iban='';
+error_ch='';
+error_nul='';
 checkbox = false;
+error_fir='';
+error_las='';
+error_user='';
+error_pass='';
+error_prov='';
+error_desc='';
+a=1;
 
   constructor(private providerService: ProvidersService, private router: Router,
               private userService: UserService, private auth: AuthenticationService) { }
@@ -53,11 +64,66 @@ checkbox = false;
 
 
   onSubmit() {
-    if(this.checkbox){
-    if(this.providers.mail.indexOf("@") == -1){
-      this.error_mail = 'Incorrect email';
+    this.error_ch='';
+   if(this.checkbox){
+   this.error_mail='';
+   this.error_prov='';
+   this.error_tel='' ;
+   this.error_iban='';
+   this.error_fir='';
+   this.error_las='';
+   this.error_user='';
+   this.error_pass='';
+   this.error_desc='';
+   this.a=1;
+
+   if (this.providers.companyName == '')
+   {
+     this.error_prov="Field Company name must be complected";
+     this.a=0;
+   }
+
+    if (this.providers.description == '')
+    {
+      this.error_desc="Field Description must be complected";
+      this.a=0;
     }
-    else{
+   if (this.providers.firstName == '')
+   {
+     this.error_fir="Field first name must be complected";
+     this.a=0;
+   }
+   if (this.providers.lastName == '')
+   {
+     this.error_las="Field last name must be complected";
+     this.a=0;
+   }
+   if (this.providers.userName == '')
+   {
+     this.error_user="Field usernane must be complected";
+     this.a=0;
+   }
+   if (this.providers.password.length <8)
+   {
+     this.error_pass="Password must be at least 8 characters";
+     this.a=0;
+   }
+
+   if(this.providers.mail.indexOf("@") == -1){
+     this.error_mail = 'Incorrect email';
+     this.a=0;
+   }
+   if (this.providers.telNumber.length != 10)
+   {
+     this.error_tel= 'Incorrect Telephone Number (10 digits needed)';
+     this.a=0;
+   }
+   if (this.providers.iban.length != 16)
+   {
+     this.error_iban= 'Incorrect Iban (16 digits needed)';
+     this.a=0;
+   }
+   else if (this.a==1){
         this.providerService.createProvider(this.providers)
           .subscribe(
           res => {
@@ -69,11 +135,13 @@ checkbox = false;
           }),
           () => {
             console.log("in ()");
-          }    
+          }
     }
   }
   else{
-    this.router.navigate(['provider-register']);
+    this.error_ch= 'Terms of use must be accepted';
+   this.router.navigate(['provider-register']);
+   this.a=0;
   }
   }
 

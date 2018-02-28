fadf140ec315402da11d2569d2fdcae1cd1fda9c
vaggosagoro <vaggosagoro@gmail.com>
2018-02-28 17:25:37 +0200

    insert database

---
+++ backend/src/main/resources/database/DDL.sql
@@ -0,0 +1,136 @@
+CREATE DATABASE softengdb;
+
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `event` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `area` varchar(255) DEFAULT NULL,
+  `street_name` varchar(255) DEFAULT NULL,
+  `street_number` int(11) DEFAULT NULL,
+  `age_group` int(11) DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  `category` int(11) DEFAULT NULL,
+  `clicks` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `eventname` varchar(255) DEFAULT NULL,
+  `initial` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  `provider_company_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKjnpyxhja0c2xxdqldclcd8nox` (`provider_company_name`),
+  CONSTRAINT `FKjnpyxhja0c2xxdqldclcd8nox` FOREIGN KEY (`provider_company_name`) REFERENCES `providers` (`company_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `parent`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `parent` (
+  `username` varchar(255) NOT NULL,
+  `area` varchar(255) DEFAULT NULL,
+  `fpoints` int(11) DEFAULT NULL,
+  `street_name` varchar(255) DEFAULT NULL,
+  `street_number` int(11) DEFAULT NULL,
+  `debit_card` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `wallet` int(11) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `parent_events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `parent_events` (
+  `parents_username` varchar(255) NOT NULL,
+  `events_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`parents_username`,`events_id`),
+  KEY `FKp8iw9t4v5lw6sj6tm7ahwoij2` (`events_id`),
+  CONSTRAINT `FK2fnr9kje5wmrveu4ikte31jvh` FOREIGN KEY (`parents_username`) REFERENCES `parent` (`username`),
+  CONSTRAINT `FKp8iw9t4v5lw6sj6tm7ahwoij2` FOREIGN KEY (`events_id`) REFERENCES `event` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `providers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `providers` (
+  `company_name` varchar(255) NOT NULL,
+  `area` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `iban` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `mail` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `street_name` varchar(255) DEFAULT NULL,
+  `street_number` int(11) DEFAULT NULL,
+  `tel_number` varchar(255) DEFAULT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`company_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_roles` (
+  `user_id` bigint(20) NOT NULL,
+  `roles_id` bigint(20) NOT NULL,
+  UNIQUE KEY `UK_amwlmdeik2qdnksxgd566knop` (`roles_id`),
+  KEY `FK55itppkw3i07do3h7qoclqd4k` (`user_id`),
+  CONSTRAINT `FK55itppkw3i07do3h7qoclqd4k` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FKj9553ass9uctjrmh0gkqsmv0d` FOREIGN KEY (`roles_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

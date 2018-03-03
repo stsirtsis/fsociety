fd883cbe1084ca72285b6c0cae7019984513abd6
stsirtsis <stsirtsis@gmail.com>
2018-03-03 20:28:47 +0200

    Event photos working.

--- backend/frontend/src/app/pages/parent-events/events-list/single-event/single-event.component.ts
+++ backend/frontend/src/app/pages/parent-events/events-list/single-event/single-event.component.ts
@@ -17,17 +17,11 @@ export class SingleEventComponent implements OnInit {
   }
 
   ngOnInit() {
-    if (this.event.description.length < 100){
-      this.shortDescription = this.event.description;
-      this.isLong = false;
-    }
-    else{
-      this.shortDescription = this.event.description.substring(0,100)+"...";
-      this.isLong = true;
-    }
+  
   }
 
   ngOnChanges(changes: SimpleChanges) {
+
     this.event = changes.event.currentValue;
     if (this.event.description.length < 100){
       this.shortDescription = this.event.description;

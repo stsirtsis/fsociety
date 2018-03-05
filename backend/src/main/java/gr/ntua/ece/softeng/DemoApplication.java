fcf74a57b1346f2b0bf5934d7d4764bf37743444
gpapama <gpapama95@gmail.com>
2018-03-05 19:37:31 +0200

    duplicates in sql fixed

--- backend/src/main/java/gr/ntua/ece/softeng/DemoApplication.java
+++ backend/src/main/java/gr/ntua/ece/softeng/DemoApplication.java
@@ -5,24 +5,11 @@ import gr.ntua.ece.softeng.entities.User;
 import gr.ntua.ece.softeng.repositories.UserRepository;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.init.DatabasePopulator;
-import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-
-import javax.sql.DataSource;
-import java.beans.PropertyVetoException;
 import java.util.Arrays;
 
 import javax.annotation.Resource;
--- backend/src/main/java/gr/ntua/ece/softeng/controllers/ProviderController.java
+++ backend/src/main/java/gr/ntua/ece/softeng/controllers/ProviderController.java
@@ -1,7 +1,6 @@
 package gr.ntua.ece.softeng.controllers;
 
 import static java.util.stream.Collectors.groupingBy;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -12,13 +11,9 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-
 import org.codehaus.jackson.map.ObjectMapper;
-
 import gr.ntua.ece.softeng.entities.*;
 import gr.ntua.ece.softeng.repositories.UserRepository;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,31 +25,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import gr.ntua.ece.softeng.entities.E;
 import gr.ntua.ece.softeng.entities.Event;
 import gr.ntua.ece.softeng.entities.Providers;
-
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 import gr.ntua.ece.softeng.repositories.ERepository;
 import gr.ntua.ece.softeng.repositories.EventRepository;
 import gr.ntua.ece.softeng.repositories.ProvidersRepository;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-
-
 import gr.ntua.ece.softeng.services.StorageService;
 
 
@@ -82,27 +63,20 @@ public class ProviderController {
 	@PostMapping(path="/addNewEvent/{providerCompanyName}")
 	@PreAuthorize("hasAuthority('PROVIDER') or hasAuthority('ADMIN')")
 	public ResponseEntity<String> addNewEvent_post (@RequestParam("event") String str, @PathVariable String providerCompanyName,@RequestParam("file") MultipartFile file) throws IOException, JSONException {
-		Providers provider = providersRepository.findByCompanyName(providerCompanyName);
-
+		Providers provider = providersRepository.findByUserName(providerCompanyName);
 		String username = provider.getUserName();
 		User user = userRepository.findByUsername(username);
-
 		for (Role role : user.getRoles()) {
 				if (role.getName().equals("BLOCKED")) {
 						return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("User is blocked by admin");
 				}
 		}
-
 		ObjectMapper mapper = new ObjectMapper();
 		Event e = mapper.readValue(str, Event.class);
 		Integer initialcap = e.getCapacity();
 		e.setClicks(0);
 		e.setInitial(initialcap);
 		e.setProvider(provider);
-
-		provider.getEvents().add(e);
-		providersRepository.save(provider);
-
 		String message = "";
 			try {
 				storageService.store(file);
@@ -110,72 +84,71 @@ public class ProviderController {
 				files.add(file.getOriginalFilename());
 
 				message = "You successfully uploaded " + file.getOriginalFilename() + "!";
+				eventRepository.save(e);
+				provider.getEvents().add(e);
+				providersRepository.save(provider);
+				Long myId = e.getId();
+				String id = Long.toString(myId);
 
-
-
-		eventRepository.save(e);
-		Long myId = e.getId();
-		String id = Long.toString(myId);
-
-		String eventname = e.getEventname();
-		String description = e.getDescription();
-		String Area = e.getArea();
-		String StreetName = e.getStreetName().replaceAll("\\s+","");
-		Integer StreetNumber = e.getStreetNumber();
-		Integer AgeGroup = e.getAgeGroup();
-		Integer capacity = e.getCapacity();
-		Integer price = e.getPrice();
-		Integer category = e.getCategory();
-		String company_name = e.getProvider().getcompanyName();
-		String date=e.getDate().toString();
-		String state="OPEN";
-		String photoUri=e.getPhotoUri();
-		String photoBody="";
-		final String TARGET_URL =
+				String eventname = e.getEventname();
+				String description = e.getDescription();
+				String Area = e.getArea();
+				String StreetName = e.getStreetName().replaceAll("\\s+","");
+				Integer StreetNumber = e.getStreetNumber();
+				Integer AgeGroup = e.getAgeGroup();
+				Integer capacity = e.getCapacity();
+				Integer price = e.getPrice();
+				Integer category = e.getCategory();
+				String company_name = e.getProvider().getcompanyName();
+				String date=e.getDate().toString();
+				String state="OPEN";
+				String photoUri=e.getPhotoUri();
+				String photoBody="";
+				final String TARGET_URL =
 	               "https://maps.googleapis.com/maps/api/geocode/json?address=";
-		final String help1= "+";
-		final String help2=",";
-		final String API_KEY =
-	               "&key=AIzaSyCi-UTmdLdEpurrr8A5Ou5I17cihpelPcI";
-		URL serverUrl = new URL(TARGET_URL+StreetNumber+help1+StreetName+help2+Area+API_KEY);
+				final String help1= "+";
+				final String help2=",";
+				final String API_KEY =
+						"&key=AIzaSyCi-UTmdLdEpurrr8A5Ou5I17cihpelPcI";
+				URL serverUrl = new URL(TARGET_URL+StreetNumber+help1+StreetName+help2+Area+API_KEY);
 
-		URLConnection urlConnection = serverUrl.openConnection();
-		HttpURLConnection httpConnection = (HttpURLConnection)urlConnection;
+				URLConnection urlConnection = serverUrl.openConnection();
+				HttpURLConnection httpConnection = (HttpURLConnection)urlConnection;
 
-		httpConnection.setRequestMethod("GET");
-		httpConnection.setRequestProperty("Content-Type", "application/json");
-		httpConnection.setDoOutput(true);
+				httpConnection.setRequestMethod("GET");
+				httpConnection.setRequestProperty("Content-Type", "application/json");
+				httpConnection.setDoOutput(true);
 
-		if (httpConnection.getInputStream() == null) {
-			   System.out.println("No stream");
-		}
+				if (httpConnection.getInputStream() == null) {
+					System.out.println("No stream");
+				}
 
-		Scanner httpResponseScanner = new Scanner (httpConnection.getInputStream());
-		String resp = "";
-		while (httpResponseScanner.hasNext()) {
-			 String line = httpResponseScanner.nextLine();
-			 resp += line;
-		}
-		httpResponseScanner.close();
+				Scanner httpResponseScanner = new Scanner (httpConnection.getInputStream());
+				String resp = "";
+				while (httpResponseScanner.hasNext()) {
+					String line = httpResponseScanner.nextLine();
+					resp += line;
+				}
+				httpResponseScanner.close();
 
-		JSONObject json = new JSONObject(resp);
+				JSONObject json = new JSONObject(resp);
 
-		JSONArray results =  json.getJSONArray("results");
-		JSONObject sessionobj=results.getJSONObject(0);
-		JSONObject geometry=sessionobj.getJSONObject("geometry");
-		JSONObject location=geometry.getJSONObject("location");
-		Double latitude=location.getDouble("lat");
-		Double longitude=location.getDouble("lng");
+				JSONArray results =  json.getJSONArray("results");
+				JSONObject sessionobj=results.getJSONObject(0);
+				JSONObject geometry=sessionobj.getJSONObject("geometry");
+				JSONObject location=geometry.getJSONObject("location");
+				Double latitude=location.getDouble("lat");
+				Double longitude=location.getDouble("lng");
 
-		eRepository.save(new E(id, eventname, description , Area , StreetName , StreetNumber , AgeGroup , capacity , price , category , company_name, latitude, longitude, date, state, photoUri, photoBody ));
+				eRepository.save(new E(id, eventname, description , Area , StreetName , StreetNumber , AgeGroup , capacity , price , category , company_name, latitude, longitude, date, state, photoUri, photoBody ));
 
-		return ResponseEntity.status(HttpStatus.OK).body(message);
+				return ResponseEntity.status(HttpStatus.OK).body(message);
+				}
+			catch (Exception e2) {
+				message = "FAIL to upload " + file.getOriginalFilename() + "!";
+				return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
 			}
-		catch (Exception e2) {
-			message = "FAIL to upload " + file.getOriginalFilename() + "!";
-			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
 		}
-	}
 
 	@PostMapping(path = "/eventsByCompany/{providerCompanyName}")
 	@PreAuthorize("hasAuthority('PROVIDER') or hasAuthority('ADMIN')")

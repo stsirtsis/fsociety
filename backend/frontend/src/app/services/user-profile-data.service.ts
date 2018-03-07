824226f1a0159a2cde4a417f16c6a292e01f6ded
stsirtsis <stsirtsis@gmail.com>
2018-03-07 16:52:27 +0200

    Changed admin page for blocking/unblocking.

--- backend/frontend/src/app/services/user-profile-data.service.ts
+++ backend/frontend/src/app/services/user-profile-data.service.ts
@@ -9,35 +9,44 @@ import {Parent} from '../interfaces/parent.interface';
 export class UserProfileDataService {
 
   private GET_URL = '/user_info';
+  private GET_USERS_URL = '/admin/all_users'
   private GET_PARENT_URL='/admin/all_parents';
   private GET_PROVIDERS_URL='/admin/all_providers';
   private GET_LOCK_URL='/admin/lock_user';
   private GET_UNLOCK_URL='/admin/unlock_user';
   private headers = new HttpHeaders({'Content-Type': 'application/json'});
-  parents: Parent[]=[];
+  //parents: Parent[]=[];
+
   constructor(private http: HttpClient, private userService: UserService) {
   }
 
   getUserData(username: string, access_token: string): Observable<any> {
     return this.http.get<any>(this.GET_URL + '?username=' + username
-      + '&access_token=' + access_token,
-      {observe: 'response'});
+    + '&access_token=' + access_token,
+    {observe: 'response'});
   }
+
+  getUsers(): Observable <any> {
+    return this.http.get<any>(this.GET_USERS_URL + '?access_token=' + this.userService.getAccessToken());
+  }
+
   getParents(): Observable <any> {
-   return this.http.get<any>(this.GET_PARENT_URL+ '?access_token=' + this.userService.getAccessToken(),{observe: 'response'});
-   }
+    return this.http.get<any>(this.GET_PARENT_URL+ '?access_token=' + this.userService.getAccessToken(),{observe: 'response'});
+  }
+
   getProviders(): Observable <any> {
     return this.http.get<any>(this.GET_PROVIDERS_URL+ '?access_token=' + this.userService.getAccessToken(),{observe: 'response'});
-    }
+  }
+
   lockUser(username: string): Observable<any> {
-        return this.http.get<any>(this.GET_LOCK_URL + '?username=' + username
-            + '?access_token=' + this.userService.getAccessToken(),
-            {observe: 'response'});}
+    return this.http.get<any>(this.GET_LOCK_URL + '?username=' + username +
+    '&access_token=' + this.userService.getAccessToken(),
+    {observe: 'response'});
+  }
 
   unlockUser(username: string): Observable<any> {
-     return this.http.get<any>(this.GET_UNLOCK_URL + '?username=' + username
-                      + '?access_token=' + this.userService.getAccessToken(),
-                      {observe: 'response'});}
-
-
-                     }
+    return this.http.get<any>(this.GET_UNLOCK_URL + '?username=' + username +
+    '&access_token=' + this.userService.getAccessToken(),
+    {observe: 'response'});
+  }
+}

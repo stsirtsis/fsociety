f9e52379b3f75d7b343b3fda9b897cae8564972a
aggelosdani <aggelosdani@gmail.com>
2018-03-07 19:40:27 +0200

    Update initialize

--- backend/src/main/java/gr/ntua/ece/softeng/DemoApplication.java
+++ backend/src/main/java/gr/ntua/ece/softeng/DemoApplication.java
@@ -1,7 +1,10 @@
 package gr.ntua.ece.softeng;
 
+import gr.ntua.ece.softeng.entities.E;
 import gr.ntua.ece.softeng.entities.Role;
 import gr.ntua.ece.softeng.entities.User;
+import gr.ntua.ece.softeng.repositories.ERepository;
+import gr.ntua.ece.softeng.repositories.EventRepository;
 import gr.ntua.ece.softeng.repositories.UserRepository;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.awt.Event;
 import java.util.Arrays;
 
 import javax.annotation.Resource;
@@ -23,7 +28,14 @@ public class DemoApplication implements CommandLineRunner{
 
     @Autowired
     private UserRepository userRepository;
-
+    
+    @Autowired
+    private ERepository eRepository;
+    
+    @Autowired
+    private EventRepository eventRepository;
+    
+    
 		@Resource
 		StorageService storageService;
     public static void main(String[] args) {
@@ -35,10 +47,39 @@ public class DemoApplication implements CommandLineRunner{
 		//	storageService.deleteAll();
 			storageService.init();
 		}
-
+		
+	
+	
     @Bean
     InitializingBean sendDatabase() {
         return () -> {
+        	
+        	eventRepository.findAll().forEach(e -> {
+        	
+        		Long myId = e.getId();
+				String id = Long.toString(myId);
+
+				String eventname = e.getEventname();
+				String description = e.getDescription();
+				String Area = e.getArea();
+				String StreetName = e.getStreetName().replaceAll("\\s+","");
+				Integer StreetNumber = e.getStreetNumber();
+				Integer AgeGroup = e.getAgeGroup();
+				Integer capacity = e.getCapacity();
+				Integer price = e.getPrice();
+				Integer category = e.getCategory();
+				String company_name = e.getProvider().getcompanyName();
+				String date=e.getDate().toString();
+				String state="OPEN";
+				String photoUri=e.getPhotoUri();
+				String photoBody=e.getPhotoBody();
+        		
+                eRepository.save(new E(id, eventname, description , Area , StreetName , StreetNumber , AgeGroup , capacity , price , category , company_name, null, null, date, state, photoUri, photoBody));
+            }
+        	);
+
+        	
+        	
             String password = org.apache.commons.codec.digest.DigestUtils.sha256Hex("admin");
             userRepository.save(new User("admin", password, Arrays.asList(new Role("ADMIN"))));
         };
